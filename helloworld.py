d1cfdda891187efe02ad65454790a45ee1a8771a
timmyreilly <timreilly@live.com>
2015-10-15 13:26:17 -0700

    added index

--- helloworld.py
+++ helloworld.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def root():
+	return 'Hello World!'
+		
+@app.route('/index')
 def index():
-		return 'Hello World!'
+	return "Hello Index"
 		
 if __name__ == "__main__":
 	app.run()
