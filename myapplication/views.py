63345730591b0d1f01808df2348252a8ca4e3372
timmyreilly <timreilly@live.com>
2015-10-19 14:22:26 -0700

    added first template

--- myapplication/views.py
+++ myapplication/views.py
@@ -2,20 +2,13 @@
 Routes and views for the flask application.
 """
 
-from flask import Flask
+from flask import render_template
 from myapplication import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'TIM'}  # fake user
-    return '''
-<html>
-  <head>
-    <title>Home Page</title>
-  </head>
-  <body>
-    <h1>Hello, ''' + user['nickname'] + '''</h1>
-  </body>
-</html>
-'''
+    user = {'nickname': 'Tim'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
